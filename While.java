c533d1c699d89e0c5294751240fb2fa5bb62e998
Guru1898 <103811373+Guru1898@users.noreply.github.com>
2022-04-16 19:32:37 +0530

    Day1 assignement

---
+++ While.java
@@ -0,0 +1,16 @@
+
+public class While {
+	public static void main(String[] args) {
+		int num = 856;
+		int rem = 0;
+		int sum = 0;
+		while (num>0) {
+			rem = num % 10;
+			sum = sum + rem;
+			num = num / 10;
+		}
+		System.out.println("Sum of the digits are " + sum);
+	}
+
+}
+	
